5c5d6bc1c6a3d0266fc956ab3aeb92e864bf7254
neeraj <neeraj@ramailo.tech>
2024-06-23 00:05:11 +0530

    day2 completed

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-
-const add = (a, b) => {
-    return a + b;
-}
-console.log(fs.readFileSync("demo.txt", "utf-8"))// this we have used synchronous file system
-
-
-
-// this is asysnchronous file system
-// fs.readFile("demo.txt", "utf-8", (err, text) => {
-//     console.log(text)
-// })
-
-console.log(add(1, 2));
+import express from 'express';
+const app = express();
+console.log('hello world');
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
 
