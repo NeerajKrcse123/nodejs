ba27d6558f606edd31c4bd2b847cacd8660f5ec1
neeraj <neeraj@ramailo.tech>
2024-06-24 00:25:02 +0530

    Day3

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import express from 'express';
-const app = express();
-console.log('hello world');
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
-});
-
+import fs from 'fs';
+import http from 'http';
+// const index = fs.readFileSync('./index.html', 'utf-8');
+// const data = { name: 'Jane', age: 25 };
+const jsondata = fs.readFileSync('./data.json', 'utf-8');
+const server = http.createServer((req, res) => {
+    // res.end('Hello World')
+    console.log(req.url);
+    // res.setHeader('Content-Type', 'application/json'); // we can set the header also
+    // res.end(JSON.stringify(data));
+    // res.end(index);
+    res.end(jsondata);
+})
+server.listen(8080, () => {
+    console.log("server started")
+})
