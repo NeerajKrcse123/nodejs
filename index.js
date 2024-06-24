88809df89db1b104b516a15f9d8569d98b085efd
neeraj <neeraj@ramailo.tech>
2024-06-24 18:08:40 +0530

    Day5 basic of middleware

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
+import express from 'express';
 import fs from 'fs';
-import http from 'http';
-const index = fs.readFileSync("index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = data.product;
+const server = express();
+// const index = fs.readFileSync('./index.html', 'utf-8');
+const data = fs.readFileSync('./data.json', 'utf-8');
+// server.get("/", (req, res) => {
+//     // res.send(index)
+//     // res.send("hello neeraj");
+//     res.send(data)
+// })
 
-const server = http.createServer((req, res) => {
 
-    if (req.url.startsWith("/products")) {
-        const id = parseInt(req.url.split("/")[2]);
-        const product = products.find(p => p.id === id);
-        res.setHeader('Content-Type', 'text/html');
-        let modifieddata = index.replace("titles", product.title).replace("bodys", product.body)
-        res.end(modifieddata);
-    }
-    // switch (req.url) {
-    //     case '/products':
-    //         res.setHeader('Content-Type', 'text/html');
-    //         let modifieddata = index.replace("titles", product.title).replace("bodys", product.body);
-    //         res.end(modifieddata);
-    //         break;
-    //     default:
-    //         res.writeHead(404, { 'Content-Type': 'text/html' });
-    //         res.write('Not Found');
-    //         res.end();
-    // }
+server.use((req, res, next) => {
+    console.log(req.method, req.url) // middleware
+    next();
 })
+
+
+server.get("/", (req, res) => {
+    res.json({ type: "get" });
+})
+// server.post("/", (req, res) => {
+//     res.json({ type: "post" });
+// })
+// server.patch("/", (req, res) => {
+//     res.json({ type: "patch" });
+// })
+// server.put("/", (req, res) => {
+//     res.json({ type: "put" });
+// })
+// server.delete("/", (req, res) => {
+//     res.json({ type: "delete" });
+// })
+
+
 server.listen(8080, () => {
-    console.log("server started")
-})
+    console.log('server is running on port 8080');
+});
