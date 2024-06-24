1d8a4f405cc37b95597dc19cc9694c1c624f5e55
neeraj <neeraj@ramailo.tech>
2024-06-24 11:34:05 +0530

    Day4 task completed learn switch

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import fs from 'fs';
 import http from 'http';
-// const index = fs.readFileSync('./index.html', 'utf-8');
-// const data = { name: 'Jane', age: 25 };
-const jsondata = fs.readFileSync('./data.json', 'utf-8');
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.product;
+
 const server = http.createServer((req, res) => {
-    // res.end('Hello World')
-    console.log(req.url);
-    // res.setHeader('Content-Type', 'application/json'); // we can set the header also
-    // res.end(JSON.stringify(data));
-    // res.end(index);
-    res.end(jsondata);
+
+    if (req.url.startsWith("/products")) {
+        const id = parseInt(req.url.split("/")[2]);
+        const product = products.find(p => p.id === id);
+        res.setHeader('Content-Type', 'text/html');
+        let modifieddata = index.replace("titles", product.title).replace("bodys", product.body)
+        res.end(modifieddata);
+    }
+    // switch (req.url) {
+    //     case '/products':
+    //         res.setHeader('Content-Type', 'text/html');
+    //         let modifieddata = index.replace("titles", product.title).replace("bodys", product.body);
+    //         res.end(modifieddata);
+    //         break;
+    //     default:
+    //         res.writeHead(404, { 'Content-Type': 'text/html' });
+    //         res.write('Not Found');
+    //         res.end();
+    // }
 })
 server.listen(8080, () => {
     console.log("server started")
