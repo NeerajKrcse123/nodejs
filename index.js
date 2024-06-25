933a41838fd6e5cac642a7f98d5ffb30c54247e4
neeraj <neeraj@ramailo.tech>
2024-06-25 18:24:08 +0530

    Day5

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 import express from 'express';
-import fs from 'fs';
 const server = express();
-// const index = fs.readFileSync('./index.html', 'utf-8');
-const data = fs.readFileSync('./data.json', 'utf-8');
-// server.get("/", (req, res) => {
-//     // res.send(index)
-//     // res.send("hello neeraj");
-//     res.send(data)
-// })
 
 
-server.use((req, res, next) => {
-    console.log(req.method, req.url) // middleware
-    next();
-})
+const auth = (req, res, next) => {
+    console.log(req.query);
+    if (req.query.password) {
+        next();
+    }
+    else {
+        res.sendStatus(401);
+    }
+}
+
+// server.use(auth); // if we  do like that then it will applicable for all routes means whatever we write below  we have written  get  and post  so here middleware will applicable for both 
 
 
-server.get("/", (req, res) => {
+// if we want to use for specific route then directly in the method
+
+
+
+
+server.get("/", auth, (req, res) => {
     res.json({ type: "get" });
 })
-// server.post("/", (req, res) => {
-//     res.json({ type: "post" });
-// })
-// server.patch("/", (req, res) => {
-//     res.json({ type: "patch" });
-// })
-// server.put("/", (req, res) => {
-//     res.json({ type: "put" });
-// })
-// server.delete("/", (req, res) => {
-//     res.json({ type: "delete" });
-// })
+server.post("/", (req, res) => {
+    res.json({ type: "post" });
+})
+
 
 
 server.listen(8080, () => {
