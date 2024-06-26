156428fd85fb0a8cfef498a4f3bf4834f5dccc48
neeraj <neeraj@ramailo.tech>
2024-06-26 17:07:37 +0530

    Day6 crud operations

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 import express from 'express';
+import fs from 'fs';
+
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.product;
 const server = express();
-
-
-const auth = (req, res, next) => {
-    console.log(req.query);
-    if (req.query.password) {
-        next();
-    }
-    else {
-        res.sendStatus(401);
-    }
-}
-
-// server.use(auth); // if we  do like that then it will applicable for all routes means whatever we write below  we have written  get  and post  so here middleware will applicable for both 
-
-
-// if we want to use for specific route then directly in the method
+server.use(express.json());
 
 
 
-
-server.get("/", auth, (req, res) => {
-    res.json({ type: "get" });
+// create api  by post
+server.post("/products", (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.json(req.body);
 })
-server.post("/", (req, res) => {
-    res.json({ type: "post" });
+
+// get api 
+server.get("/products", (req, res) => {
+    res.json(products); // here we will get all the products
 })
+
+server.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const product = products.find(product => product.id === parseInt(id)); // here we will get the specific products by id example /products/3
+    res.json(product);
+})
+
+
+server.put("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const productIndex = products.findIndex(product => product.id === parseInt(id));
+    products.splice(productIndex, 1, { ...req.body, id: parseInt(id) });
+    res.json(products);
+
+})
+server.patch("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const productIndex = products.findIndex(product => product.id === parseInt(id));
+    const product = products[productIndex];
+    products.splice(productIndex, 1, { ...product, ...req.body });
+    res.json(products);
+
+})
+
+server.delete("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const productIndex = products.findIndex(product => product.id === parseInt(id));
+    products.splice(productIndex, 1);
+    res.json(products);
+
+})
+
+
+
+
 
 
 
